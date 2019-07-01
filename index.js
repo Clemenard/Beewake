65329f882eee6ce1031e0c132f21a42ec013ff38
Clément Menard <cm.13@hotmail.fr>
2019-07-01 15:08:34 +0200

    Html/Css finalize
    Adjust the layout to match the .png

--- index.js
+++ index.js
@@ -33,42 +33,34 @@ for(user of request.data){
   userObject.addUser(users)
 }
 request.data = users
-//Adding a new user
+// Adding a new user
 var newUser= new User("contact@clement-menard.fr","Clément","Menard","https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/10172624_10152106710023106_8746587590316635122_n.jpg?_nc_cat=110&_nc_oc=AQlCD8vEu12rYftxm6T17gITt5coh6n979GNTSJqBUlbq0ktXEJmNsqHRul_Wp2sDxc&_nc_ht=scontent-cdg2-1.xx&oh=3e3d56bc6e6e1e58335afd328cb58b00&oe=5DB25AF1");
 newUser.addUser(request.data)
 //Delete an user
 
-newUser.deleteUser(3,request.data)
+userObject.deleteUser(3,request.data)
 
 //Update an user
 var changes = {"email":"clement.menard.13@gmail.com"}
-newUser.updateUser(7,changes,request.data)
+userObject.updateUser(7,changes,request.data)
 
 
 Cookies.set('users',request);
 
 
 // Html/Css layout
-var content ='<table class="table table-dark table-striped">'
- content +='<tr>'
-var keys = Object.keys(request)
-for(  key of keys ) {
- content +='<th scope="col">'+ key +'</th>';
+var content = '<div class="container">'
+for(user of request.data){
+ content +='<div class="row" class="user">'
+content +=  '<img class="col-4" src="' + user.getAvatar() + '" alt="Card image cap">'
+content +=  '<div class="col-8">'
+content +=    '<h5 class="card-title">' + user.getFirst_name().toUpperCase() + ' '+ user.getLast_name().toUpperCase() +'</h5>'
+content +=    '<p class="card-text">'+ user.getEmail() +'</p>'
+content +=  '</div>'
+content +='</div>'
+content+='<hr />'
 }
- content +='</tr>'
- for( var user of request.data ) {
- content +='<tr>'
-    for( key in user) {
-      if(key == 'avatar'){
-       content +=  '<td><img src="' + user[key] + '" alt="" /></td>'
-      }
-      else {
- content +=  '<td>' + user[key] + '</td>'
-}
-}
- content +='</tr>'
-}
- content +='</table>'
+ content+='</div>'
 $('#content').html(content);
 
 } ) ;
