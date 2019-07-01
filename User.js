40485e20f013b4b285eb469ffc6e1935812f6b90
Clément Menard <cm.13@hotmail.fr>
2019-07-01 12:53:35 +0200

    Class User added
    - Class User, getters ans setters, crud
    - Demo code for its use in index.js

---
+++ User.js
@@ -0,0 +1,67 @@
+class User {
+  constructor(email,first_name,last_name,avatar,id=null) {
+    this.setEmail(email)
+    this.setFirst_name(first_name)
+    this.setLast_name(last_name)
+  this.setAvatar(avatar)
+  this.setId(id)
+
+  return this
+  }
+  setEmail(email){this.email=email}
+  getEmail(){return this.email}
+
+  setFirst_name(first_name){this.first_name=first_name}
+  getFirst_name(){return this.first_name}
+
+  setLast_name(last_name){this.last_name=last_name}
+  getLast_name(){return this.last_name}
+
+  setAvatar(avatar){this.avatar=avatar}
+  getAvatar(){return this.avatar}
+
+  setId(id){this.id=id}
+  getId(){return this.id}
+
+getUser(id,users){
+  for(var user of users){
+
+if(user.id==id){ return user}
+}
+return false
+}
+
+
+   addUser(users){
+     if(this.getId()==null){
+     var idmax = 0
+     for(var user of users){
+       if(user.id>idmax){
+         idmax=user.id
+       }
+
+     }
+     this.setId(idmax+1)
+   }
+
+users.push(this);
+return users;
+  }
+
+  deleteUser(users){
+    var i=0
+    for(var user of users){
+if(user.id==this.id){
+  users.splice(i,1)
+  break
+}
+i++
+  }
+}
+
+updateUser(users){
+this.deleteUser(users)
+this.addUser(users)
+}
+
+}
--- index.js
+++ index.js
@@ -1,39 +1,60 @@
 $(document).ready(function(){
-   if(Cookies.get('users'))
-   {}
-   else{
- $.ajax({
-   url: 'https://reqres.in/api/users?page=1&per_page=6',
-   method: 'get',
-   dataType: 'json',
-   success: function(request){
+   if(!Cookies.get('users')){
 
-  Cookies.set('users',request);
+     $.ajax({
+       url: 'https://reqres.in/api/users?page=1&per_page=6',
+       method: 'get',
+       dataType: 'json',
+       success: function(request){
+
+Cookies.set('users',request);
 }
 });
- };
-users = Cookies.getJSON('users');
 
- var content ='<table class="table table-dark table-striped">'
-  content +='<tr>'
- var keys = Object.keys(users.data[0])
+}
+request = Cookies.getJSON('users');
+
+//Create User objects with the data
+var users=[]
+for(user of request.data){
+  userObject = new User(user.email,user.first_name,user.last_name,user.avatar,user.id);
+  userObject.addUser(users)
+}
+request.data = users
+
+//Adding a new user
+var newUser= new User("contact@clement-menard.fr","Clément","Menard","https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/10172624_10152106710023106_8746587590316635122_n.jpg?_nc_cat=110&_nc_oc=AQlCD8vEu12rYftxm6T17gITt5coh6n979GNTSJqBUlbq0ktXEJmNsqHRul_Wp2sDxc&_nc_ht=scontent-cdg2-1.xx&oh=3e3d56bc6e6e1e58335afd328cb58b00&oe=5DB25AF1");
+newUser.addUser(request.data)
+
+//Delete an user
+oldUser=newUser.getUser(3,request.data)
+oldUser.deleteUser(request.data)
+
+//Update an user
+changingUser = newUser.getUser(7,request.data)
+changingUser.setEmail("clement.menard.13@gmail.com")
+changingUser.updateUser(request.data)
+
+// Html/Css layout
+var content ='<table class="table table-dark table-striped">'
+ content +='<tr>'
+var keys = Object.keys(request)
 for(  key of keys ) {
-  content +='<th scope="col">'+ key +'</th>';
- }
-  content +='</tr>'
-  for( var user of users.data ) {
-  content +='<tr>'
-     for( key in user) {
-       if(key == 'avatar'){
-        content +=  '<td><img src="' + user[key] + '" alt="" /></td>'
-       }
-       else {
-  content +=  '<td>' + user[key] + '</td>'
+ content +='<th scope="col">'+ key +'</th>';
+}
+ content +='</tr>'
+ for( var user of request.data ) {
+ content +='<tr>'
+    for( key in user) {
+      if(key == 'avatar'){
+       content +=  '<td><img src="' + user[key] + '" alt="" /></td>'
+      }
+      else {
+ content +=  '<td>' + user[key] + '</td>'
 }
 }
-  content +='</tr>'
+ content +='</tr>'
 }
-  content +='</table>'
+ content +='</table>'
 $('#content').html(content);
-
 } ) ;
