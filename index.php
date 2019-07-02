9d16911e0d7f634794d6363f39b06d071da54711
Clemenard <contact@clement-menard.fr>
2019-07-02 14:55:43 +0200

    Directory organization
    Ordered files by language, + index & readme on the root

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $pdo = new PDO(
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
   <title>Beewake : List of users</title>
 </head>
 
@@ -31,6 +31,6 @@ src="https://code.jquery.com/jquery-3.3.1.min.js"
 integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
 crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
-<script src="User.js"></script>
-<script src="index.js"></script>
+<script src="js/User.js"></script>
+<script src="js/index.js"></script>
 </html>
